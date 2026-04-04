ef5cc0b7d8cbdfa3be3f4d60684069871cc5ad03
Purine <purinekaka@70@gmail.com>
2026-04-04 14:20:26 +0300

    Remove Firebase Functions push path

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-/* global importScripts, firebase */
-
-const APP_CACHE = "tamu-app-v30";
+const APP_CACHE = "tamu-app-v31";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -104,36 +102,6 @@ self.addEventListener("fetch", (event) => {
       });
     }),
   );
-});
-
-importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging-compat.js");
-
-firebase.initializeApp({
-  apiKey: "AIzaSyApi2RjwEXEW2mTwCMiYERKUNAYL8Toddk",
-  authDomain: "tamuexpress-26e4a.firebaseapp.com",
-  projectId: "tamuexpress-26e4a",
-  storageBucket: "tamuexpress-26e4a.appspot.com",
-  messagingSenderId: "202068792631",
-  appId: "1:202068792631:web:8a9b97028ebd7a45af76b2",
-});
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  const data = payload.data || {};
-  const title = data.title || "New notification";
-  const body = data.body || "You have a new update from Tamu Express.";
-
-  self.registration.showNotification(title, {
-    body,
-    icon: APP_ICON,
-    badge: APP_ICON,
-    data: {
-      link: resolveNotificationLink(data.link),
-    },
-    tag: data.tag || undefined,
-  });
 });
 
 self.addEventListener("notificationclick", (event) => {
